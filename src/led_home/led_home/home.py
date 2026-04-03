d3d87cbea5c1085d573f362c867acbaf5a0a3c2b
Jair-F <jair.fehlauer@gmail.com>
2026-04-03 18:50:27 +0000

    works with webpage

--- src/led_home/led_home/home.py
+++ src/led_home/led_home/home.py
@@ -1,24 +1,52 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg._color_rgba import ColorRGBA
+from flask import Flask, render_template, request
+import threading
+
+app = Flask(__name__)
+selected_color_hex = "#ffffff"
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    global selected_color_hex
+    if request.method == 'POST':
+        # Get color from the "selected_color" input field
+        selected_color_hex = request.form.get('selected_color')
+        print(f"Flask received: {selected_color_hex}")
+        
+    return render_template('index.html', color=selected_color_hex)
 
 class HomeNode(Node):
     def __init__(self):
         super().__init__("home_node")
         self._publisher = self.create_publisher(ColorRGBA, 'cmd_color', 1)
-
         self._timer = self.create_timer(1, self._send_color_callback)
-    
+
+        self._flask_thread = threading.Thread(target=lambda: app.run('0.0.0.0', port=1234, debug=False, use_reloader=False), daemon=True)
+        self._flask_thread.start()
+        self.get_logger().info('Started flask thread in background')
+
     def _send_color_callback(self):
-        color = ColorRGBA(r=1., g=1., b=1., a=1.)
-        self.get_logger().info(F"sending color {color} command to LED")
+        global selected_color_hex
+        h = selected_color_hex.lstrip('#')
+        r, g, b = tuple(int(h[i:i+2], 16)/255.0 for i in (0, 2, 4))
+        color = ColorRGBA()
+        color.r = float(r)
+        color.g = float(g)
+        color.b = float(b)
+        color.a = 1.0
+        self.get_logger().info(f"ROS 2 Publishing: R:{r:.2f} G:{g:.2f} B:{b:.2f}")
         self._publisher.publish(color)
 
 
 def main(args=None):
     rclpy.init(args=args)
     node = HomeNode()
-    rclpy.spin(node)
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
     node.destroy_node()
     rclpy.shutdown()
 
